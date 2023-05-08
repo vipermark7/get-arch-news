0c46f1a723d8c7c02009d6cfd63dd51caf49a37c
Shaffan <shaffan1996@gmail.com>
2023-05-08 00:50:16 -0400

    removed unneeded return

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,6 @@ fn main() {
 fn parse_xml() -> Result<Response, Error> {
     let arch_news_url = String::from("https://archlinux.org/feeds/news/");
     let res = reqwest::blocking::get(arch_news_url);
-    return res;
 }
 
 
