5c71fa4eeb27c549dbca572b977367c12096e77e
Shaffan <shaffan1996@gmail.com>
2023-05-08 00:38:39 -0400

    removed unnecessary mut

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() {
 
 fn parse_xml() -> Result<Response, Error> {
     let arch_news_url = String::from("https://archlinux.org/feeds/news/");
-    let mut res = reqwest::blocking::get(arch_news_url);
+    let res = reqwest::blocking::get(arch_news_url);
     return res;
 }
 
