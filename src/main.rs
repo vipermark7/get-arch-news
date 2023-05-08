844adc9ba04d9d101846a1e42f557e99b65c0e96
Shaffan <shaffan1996@gmail.com>
2023-05-08 00:37:46 -0400

    got rust to spit out raw XML! :D

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+//! `cargo run --example blocking --features=blocking`
+extern crate reqwest;
+extern crate minidom;
+
+// use minidom::Element;
+// use error_chain::error_chain;
+// use std::io::Read;
+
+
+use reqwest::{blocking::Response, Error};
+
+
 fn main() {
     println!("Hello, world!");
+    match parse_xml() {
+        Ok(parsed) => print!("{}", parsed.text().unwrap()),
+        Err(e) => panic!("{}", e),
+    }
+}   
+
+fn parse_xml() -> Result<Response, Error> {
+    let arch_news_url = String::from("https://archlinux.org/feeds/news/");
+    let mut res = reqwest::blocking::get(arch_news_url);
+    return res;
 }
+
+
+
